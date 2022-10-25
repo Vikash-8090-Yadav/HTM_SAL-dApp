7d4bb7b7cbd1a1eadf1da8b461711965e846cf00
Vikash <yadav621gzp@gmail.com>
2022-10-25 18:19:22 +0530

    added

--- Component/Cool.js
+++ Component/Cool.js
@@ -1,5 +1,5 @@
 import Image from "next/image"
-import hero form "../images/2.png";
+import hero from "../images/2.png";
 export default function  Cat() {
   return(
     <Image src ={hero} width ={1950} height = {660}
