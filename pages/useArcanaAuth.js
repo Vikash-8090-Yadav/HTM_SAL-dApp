518a328901772c305185f219d41d2b23452bcf75
Vikash Yadav <yadav621gzp@gmail.com>
2022-11-13 23:29:12 +0530

    Update useArcanaAuth.js

--- pages/useArcanaAuth.js
+++ pages/useArcanaAuth.js
@@ -6,7 +6,7 @@ import { useEffect, useState } from "react";
 
 const appId = '3B01e48556c35033A3Df7BbF8F932Ee42b0dF557';
 
-let  auth = new AuthProvider(appId);;
+let  auth = new AuthProvider(appId);
 
 function useArcanaAuth() {
   const [initialized, setInitialized] = useState(false);
