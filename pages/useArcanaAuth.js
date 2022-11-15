71fc8bdb829bbe457517ab3a996417c131b89006
Vikash Yadav <yadav621gzp@gmail.com>
2022-11-16 02:29:17 +0530

    Update useArcanaAuth.js

--- pages/useArcanaAuth.js
+++ pages/useArcanaAuth.js
@@ -78,15 +78,24 @@ import { useEffect, useState } from "react";
 
 const appId = "3B01e48556c35033A3Df7BbF8F932Ee42b0dF557";
 
-let auth = new AuthProvider(appId);
+let auth;
+// let auth = new AuthProvider(appId);
 
 function useArcanaAuth() {
   const [initialized, setInitialized] = useState(false);
 
+  
   const initializeAuth = async () => {
-    await auth.init({ appMode: 2, position: "right" });
-    setInitialized(true);
-  };
+    if (!auth) {
+      auth = new AuthProvider(appId);
+      await auth.init({ appMode: 2, position: "right" });
+      setInitialized(true);
+    }
+  }
+//   const initializeAuth = async () => {
+//     await auth.init({ appMode: 2, position: "right" });
+//     setInitialized(true);
+//   };
 
   //Check isLoggedIn
   const isLoggedIn = async () => {
