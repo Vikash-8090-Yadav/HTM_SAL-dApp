53b7bd8045fbc423943349dd5eec9cadf3e4bce6
Ritik kumar <87005719+ritik9931@users.noreply.github.com>
2022-11-18 23:00:59 +0530

    Update Cool.js

--- Component/Cool.js
+++ Component/Cool.js
@@ -1,8 +1,12 @@
+import { dividerClasses } from "@mui/material";
 import Image from "next/image"
 import hero from "../images/2.png";
 export default function  Cat() {
   return(
-    <Image src ={hero} width ={1950} height = {660}
+    <div  className="hero">
+    <Image src ={hero} width ={4000} height = {1300}
+
     />
+    </div>
   )
 }
